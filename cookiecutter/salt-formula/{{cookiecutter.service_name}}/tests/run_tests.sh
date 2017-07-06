3c68ec46e2f6de6b21348dfd27a86cf4e6192047
Dennis Dmitriev <ddmitriev@mirantis.com>
2017-07-06 21:10:34 +0300

    Update run_tests.sh to the latest revision
    Change-Id: I9484522ee2e250280a15e7c39a7f6682316ad097

--- cookiecutter/salt-formula/{{cookiecutter.service_name}}/tests/run_tests.sh
+++ cookiecutter/salt-formula/{{cookiecutter.service_name}}/tests/run_tests.sh
@@ -12,6 +12,7 @@ FORMULA_META_DIR=${CURDIR}/../${FORMULA_NAME}/meta
 PILLARDIR=${PILLARDIR:-${CURDIR}/pillar}
 BUILDDIR=${BUILDDIR:-${CURDIR}/build}
 VENV_DIR=${VENV_DIR:-${BUILDDIR}/virtualenv}
+MOCK_BIN_DIR=${MOCK_BIN_DIR:-${CURDIR}/mock_bin}
 DEPSDIR=${BUILDDIR}/deps
 
 SALT_FILE_DIR=${SALT_FILE_DIR:-${BUILDDIR}/file_root}
@@ -39,6 +40,15 @@ setup_virtualenv() {
     virtualenv $VENV_DIR
     source ${VENV_DIR}/bin/activate
     pip install salt${PIP_SALT_VERSION}
+}
+
+setup_mock_bin() {
+    # If some state requires a binary, a lightweight replacement for
+    # such binary can be put into MOCK_BIN_DIR for test purposes
+    if [ -d "${MOCK_BIN_DIR}" ]; then
+        PATH="${MOCK_BIN_DIR}:$PATH"
+        export PATH
+    fi
 }
 
 setup_pillar() {
@@ -122,6 +132,7 @@ prepare() {
     [ -d ${BUILDDIR} ] && mkdir -p ${BUILDDIR}
 
     which salt-call || setup_virtualenv
+    setup_mock_bin
     setup_pillar
     setup_salt
     install_dependencies
@@ -144,6 +155,13 @@ run() {
               || (log_err "Failed to render meta ${meta} using pillar ${FORMULA_NAME}.${state_name}"; exit 1)
             cat ${SALT_CACHE_DIR}/${meta_name}
         done
+    done
+}
+
+real_run() {
+    for pillar in ${PILLARDIR}/*.sls; do
+        state_name=$(basename ${pillar%.sls})
+        salt_run --id=${state_name} state.sls ${FORMULA_NAME} || (log_err "Execution of ${FORMULA_NAME}.${state_name} failed"; exit 1)
     done
 }
 
@@ -171,6 +189,9 @@ case $1 in
         ;;
     run)
         run
+        ;;
+    real-run)
+        real_run
         ;;
     *)
         prepare
