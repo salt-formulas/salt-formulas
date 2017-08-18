2339f6bff0cadb7ab81b58cf9e986fd334b5f292
Petr Michalec <epcim@apealive.net>
2017-08-18 12:47:49 +0200

    update model verification scripts

--- deploy/model/verify.sh
+++ deploy/model/verify.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-eval $(find / -maxdepth 1 -name '*.env' | xargs -0 -n1 --no-run-if-empty echo source)
-eval $(find . -maxdepth 1 -name '*.env' | xargs -0 -n1 --no-run-if-empty echo source)
 export MASTER_HOSTNAME=${MASTER_HOSTNAME:-`hostname -f`}
+cd /srv/salt/scripts; git pull -r || true; source bootstrap.sh || exit 1
 
 # BOOTSTRAP
 if [[ $BOOTSTRAP =~ ^(True|true|1|yes)$ ]]; then
-  # WORKAROUNDs for Kitchen
+  # workarounds for kitchen
   test ! -e /tmp/kitchen  || (mkdir -p /srv/salt/reclass; rsync -avh /tmp/kitchen/ /srv/salt/reclass)
-  # work in salt root
   cd /srv/salt/reclass
-  # clone system-level if missing
+  # clone latest system-level if missing
   if [[ -e .gitmodules ]] && [[ ! -e classes/system/linux ]]; then
-    git submodule update --init --recursive || true
+    git submodule update --init --recursive --remote || true
   fi
-  ls -la /srv/salt/reclass
+  source_local_env
   /srv/salt/scripts/bootstrap.sh
 fi
 
 # VERIFY
 export BOOTSTRAP_SALTSTACK=False
+export RECLASS_IGNORE_CLASS_NOTFOUND=False
+#system_config_master &&\
 cd /srv/salt/reclass &&\
-source /srv/salt/scripts/bootstrap.sh &&\
-system_config_master &&\
 if [[ -z "$1" ]] ; then
   verify_salt_master &&\
   verify_salt_minions
